21b49ea31cdcd27af83c905c9690b17f07cbdca7
yooknow <yooknow85@naver.com>
2022-12-02 00:14:27 +0900

    `

--- c_02_03.py
+++ c_02_03.py
@@ -27,7 +27,45 @@ class Car():
         print('Car Detail Info : {} {}'.format(
             self._company, self._details.get('price')))
 
+    def get_price(self) -> str:
+        return 'before car price -> company:{},price:{}'.format(self._company, self._details.get('price'))
+
+    def get_price_calc(self):
+        return 'before car price -> company:{},price:{}'.format(self._company, self._details.get('price') * Car.price_per_raise)
+
+    # 클래스 메소드
+    @classmethod
+    def raise_price(cls, per):
+        if per <= 1:
+            print('1 or more')
+            return
+        cls.price_per_raise = per
+
+    # static 메소드 (인자값은 아무것도 받지 않는다.)
+    # 클래스 메소드 보다는 유연.. 하다
+    @staticmethod
+    def is_bmw(inst):
+        if inst._company == 'bmw':
+            return 'this car is {}'.format(inst._company)
+        return 'this car is not bmw'
+
 
 # self의 의미
 car1 = Car('페라리', {'color': 'white', 'horsepower': 400, 'price': 8000})
 car2 = Car('bmw', {'color': 'black', 'horsepower': 300, 'price': 5000})
+
+# 가격 인상전
+print(car1.get_price())
+print(car2.get_price())
+
+# 클래스 메소드 활용
+Car.raise_price(1.4)
+
+# 가격 인상후
+print(car1.get_price_calc())
+print(car2.get_price_calc())
+
+
+# static 메소드 활용
+# print(car1.is_bmw(car1))
+print(Car.is_bmw(car2))
